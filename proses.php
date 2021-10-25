07da925925accd68ff79e52708c2e3f296cc0bc8
syafiqajefri <syafiqajefri1705@gmail.com>
2021-10-25 13:01:20 +0800

    Pelbagai Soalan

---
+++ proses.php
@@ -0,0 +1,26 @@
+<?php
+#$nama = $_POST['nama'];
+/*$nama = test_input($_POST['nama']);
+
+
+echo "[$nama]";
+
+function test_input($data)
+{
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}*/
+
+ $kata = $_POST['kata'];
+ if (starlen($kata) <6 ) {
+     echo 'Maaf kata laluan terlalu pendek ';
+ } elseif(...) {
+    # mesti ada huruf besar
+    # mesti ada huruf kecil
+    # mesti ada nombor
+    # mesti ada simbol
+     } else {
+         echo 'Kata laluan diterima;'
+}
